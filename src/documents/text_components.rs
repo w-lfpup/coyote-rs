9719e71f25592dd2ac66f4b596450df810e7804a
Taylor Vann <brian.t.vann@gmail.com>
2025-11-29 19:21:13 -0800

    use rules to decide indentation on alt text

--- src/documents/text_components.rs
+++ src/documents/text_components.rs
@@ -1,4 +1,5 @@
 use crate::documents::tag_info::{TagInfo, TextFormat};
+use crate::template_steps::RulesetImpl;
 use std::cmp;
 
 fn get_index_of_first_char(text: &str) -> usize {
@@ -57,7 +58,12 @@ fn get_largest_common_space_index(texts: &[&str]) -> usize {
     space_index
 }
 
-pub fn push_alt_text_component(results: &mut String, text: &str, tag_info: &TagInfo) {
+pub fn push_alt_text_component(
+    results: &mut String,
+    rules: &dyn RulesetImpl,
+    text: &str,
+    tag_info: &TagInfo,
+) {
     if tag_info.banned_path {
         return;
     }
@@ -95,12 +101,16 @@ pub fn push_alt_text_component(results: &mut String, text: &str, tag_info: &TagI
     let last = texts[texts.len() - 1];
     results.push('\n');
 
-    let indent_offset = match tag_info.inline_el {
-        true => 0,
-        _ => 1,
-    };
+    // should be based on rules but
+    if rules.respect_indentation() {
+        let indent_offset = match tag_info.inline_el {
+            true => tag_info.indent_count,
+            _ => tag_info.indent_count - 1,
+        };
 
-    results.push_str(&"\t".repeat(tag_info.indent_count - indent_offset));
+        results.push_str(&"\t".repeat(indent_offset));
+    }
+
     results.push_str(last.trim())
 }
 
