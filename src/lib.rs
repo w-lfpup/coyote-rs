8fe531a4483cd7431458fc1847b59c5c95c89c90
Taylor Vann <brian.t.vann@gmail.com>
2026-01-20 15:46:33 -0800

    reduce exports

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,5 @@ mod errors;
 mod template_steps;
 
 pub use components::*;
-pub use document_builders::{DocumentParams, Html, HtmlCssOnly, HtmlOnly, TemplateBuilder, Xml};
-pub use documents::{TemplateBuilderImpl, compose_string};
+pub use document_builders::{DocumentParams, Html, HtmlCssOnly, HtmlOnly, Xml};
 pub use errors::*;
-pub use template_steps::{RulesetImpl, TemplateSteps, compose};
